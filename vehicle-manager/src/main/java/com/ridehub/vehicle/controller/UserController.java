ffc83d36bb47f5ef57b8a58f18baa679f80acbac
DinishaReddy <dinishasatti@gmail.com>
2026-03-16 08:59:52 -0600

    Add user and vehicle controllers

--- vehicle-manager/src/main/java/com/ridehub/vehicle/controller/UserController.java
+++ vehicle-manager/src/main/java/com/ridehub/vehicle/controller/UserController.java
@@ -1,10 +1,8 @@
-package com.ridehub.vehicle.controller;
+package com.ridehub.vehicle.controllers;
 
-
+import com.ridehub.vehicle.exceptions.UserNotFoundException;
 import com.ridehub.vehicle.model.User;
-import com.ridehub.vehicle.service.UserService;
-import com.ridehub.vehicle.exception.UserNotFoundException;
-
+import com.ridehub.vehicle.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +17,12 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
-        User registeredUser = userService.register(user);
-        return ResponseEntity.ok(registeredUser);
+        try {
+            User registeredUser = userService.register(user);
+            return ResponseEntity.ok(registeredUser);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{username}")
@@ -30,20 +32,34 @@ public class UserController {
             return ResponseEntity.ok(user);
         } catch (UserNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> updateUser(
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestBody User user) {
-
+    public ResponseEntity<?> updateUser(@RequestParam String username,
+                                        @RequestParam String password,
+                                        @RequestBody User user) {
         try {
             User updatedUser = userService.updateUser(username, user, password);
             return ResponseEntity.ok(updatedUser);
         } catch (UserNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getUserProfile(@RequestParam String username) {
+        try {
+            User user = userService.getUserByUsername(username);
+            return ResponseEntity.ok(user);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
---
+++ vehicle-manager/src/main/java/com/ridehub/vehicle/controller/VehicleController.java
@@ -0,0 +1,72 @@
+package com.ridehub.vehicle.controllers;
+
+import com.ridehub.vehicle.exceptions.UserNotFoundException;
+import com.ridehub.vehicle.model.Vehicle;
+import com.ridehub.vehicle.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/vehicles")
+public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerVehicle(@RequestBody Vehicle vehicle, @RequestParam String username) {
+        try {
+            Vehicle registeredVehicle = vehicleService.registerVehicle(vehicle, username);
+            return ResponseEntity.ok(registeredVehicle);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateVehicle(@PathVariable Long id,
+                                           @RequestBody Vehicle vehicle,
+                                           @RequestParam String username) {
+        try {
+            Vehicle updatedVehicle = vehicleService.updateVehicle(id, vehicle, username);
+            return ResponseEntity.ok(updatedVehicle);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/by-username/{username}")
+    public ResponseEntity<?> getVehiclesByUsername(@PathVariable String username) {
+        try {
+            List<Vehicle> vehicles = vehicleService.getVehiclesByUsername(username);
+            return ResponseEntity.ok(vehicles);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteVehicle(@RequestParam String registrationNumber,
+                                           @RequestParam String username) {
+        try {
+            vehicleService.deleteVehicleByRegistrationNumber(registrationNumber, username);
+            return ResponseEntity.ok("Vehicle deleted successfully");
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
